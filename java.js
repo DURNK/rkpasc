42b41ad0f6eb081323b2465b14c6dee0a8aa2502
DURNK <durnker@gmail.com>
2022-11-28 23:25:00 +0200

    reset functionality

--- java.js
+++ java.js
@@ -4,7 +4,12 @@ let playerChoice; //global variable that can be modified by the
                     // getPlayerChoice function and passed to playRound
 
 //Game gets stuck after being reset, maybe we're not resetting everything
-                
+         
+function sleep(functionToExecute, delay){
+    let timeoutId = setTimeout(functionToExecute(), delay);
+    return timeoutId;
+  }
+
 function getComputerChoice() {
 let computerChoice;
 const pickings = ["rock","paper","scissors"];
@@ -14,36 +19,36 @@ console.log("computer choice is: " + computerChoice);
 return computerChoice;
 }
 
-function gameReset() {
-    const reset = document.querySelector('.header')
-    reset.style.removeProperty = 'pointer-events';
 
-    reset.addEventListener('click', () => {
-
-        getPlayerChoice();
-});
-
-}
 
 function getPlayerChoice() {
     const buttons = document.querySelector(".buttons"); //selects the buttons div
     buttons.style.cssText = 'pointer-events: ';
-
     buttons.addEventListener('click', (e) => {    
     const ticker = document.querySelector('.ticker')
-    console.log(e.target.className); //e is the location of the selected button and classname is the name of the selected class
+    //console.log(e.target.className); //e is the location of the selected button and classname is the name of the selected class
     playerChoice = e.target.className; 
     
     
     playRound();
 
 
-    if (playerScore == 5 || computerScore == 5) {
-        buttons.style.cssText = 'pointer-events: none';
+    if (playerScore >= 5 || computerScore >= 5) {
         ticker.textContent = `Match ended, Scores: Player ${playerScore} and Comp ${computerScore} `;
         playerScore = 0;
-        computerScore = 0;
-        gameReset();
+        computerScore = 0; 
+        buttons.style.cssText = 'pointer-events: none ';
+        console.log(playerScore);
+        console.log(computerScore);
+    setTimeout(() => {ticker.textContent = 'Resetting...';
+    console.log("Reset");
+    }, 2000);
+    setTimeout(() => {
+        ticker.textContent = `Scores: Player ${playerScore} and Comp ${computerScore} `;
+        buttons.style.cssText = 'pointer-events:';
+
+    }, 6000);
+          
     }
     });
    
@@ -58,6 +63,8 @@ const ticker = document.querySelector('.ticker')
 
     if (playerChoice === computerChoice) {
         ticker.textContent = 'DRAW!';
+        console.log("Draw" + playerChoice + computerChoice);
+
 
     }
 
@@ -73,14 +80,17 @@ const ticker = document.querySelector('.ticker')
 
     }
 
-    else {
+    else  if (
+        (computerChoice === "scissors" && playerChoice === "paper") 
+    || (computerChoice === "rock" && playerChoice === "scissors") 
+    || (computerChoice === "paper" && playerChoice === "rock"))
+    
+    {
         console.log("Computer wins 1 point!" + playerChoice + computerChoice);
         ticker.textContent = "Computer wins 1 point!";
+        computerScore += 1;
         console.log(computerScore);
 
-
-        
-        computerScore += 1;
 
     }
 }
@@ -94,49 +104,3 @@ getPlayerChoice();
 
 
 
-/*function game() {
-    for (let i = 0; i<5; i++) {
-        playRound();
-        console.log(playerScore + ' ' + computerScore)
-    }
-
-
-    if(playerScore === computerScore) {
-        console.log("Draw, nobody wins the match!")
-    }
-    else if(playerScore > computerScore){
-        console.log("Player Wins the Match!")
-    }
-    else {
-        console.log("Computer Wins the Match!")
-    }
-}
-game();
-*/
-/*const rock = document.querySelector(".rock");
- rock.addEventListener('click', () => 
- {
-    let playerChoice = "rock";
-    console.log(playerChoice);
-    return playerChoice;
-    
- });
-  
-const paper = document.querySelector(".paper");
- paper.addEventListener('click', () => 
- {
-    let playerChoice = "paper";
-    console.log(playerChoice);
-    return playerChoice;
-    
- });
-
- const scissors = document.querySelector(".scissors");
- scissors.addEventListener('click', () => 
- {
-    let playerChoice = "scissors";
-    console.log(playerChoice);
-    return playerChoice;
-    
- });
- */
